2d81920b7403d807e8fc8a8546df7b2759f895b7
marktipton <marktipton95@gmail.com>
2024-10-25 13:48:56 -0500

    added github and email options to auth.ts file

--- auth.ts
+++ auth.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,7 +8,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      }
+    })
+  ],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
