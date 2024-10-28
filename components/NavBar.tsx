6a1bc8c392f1a9f56e5f2a3f38633f4d1f87cdf9
marktipton <marktipton95@gmail.com>
2024-10-28 13:37:38 -0500

    added solid folder

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FolderIcon } from "@heroicons/react/24/outline"
+import { FolderIcon } from "@heroicons/react/24/solid"
 
 const NavBar: React.FC = () => {
   const pathname = usePathname(); // Get the current pathname
