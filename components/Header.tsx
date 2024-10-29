ea93f9992e734c21b806924dc3794b0955a27dc4
marktipton <marktipton95@gmail.com>
2024-10-29 13:28:24 -0500

    logout button now centered vertically

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,22 @@ export const Header = () => {
   const { data: session } = useSession();
   if (session) {
     return (
-      <div className='bg-teal h-10 text-searchBlue flex'>
-        <FilmIcon />
+      <div className='bg-teal h-10 text-searchBlue flex items-center'>
+        <FilmIcon className='w-5 h-5'/>
         <span className='text-xl font-bold'>Cinema Guru</span>
         <div>
           {`Hello, ${session.user?.email}`}
         </div>
-        <button
-          onClick={() => signOut()}
-          className='h-10 flex'
-        >
-          <ArrowRightStartOnRectangleIcon className='w-5 h-5'/>
-          <span>Logout</span>
-        </button>
+        <div>
+          <button
+            onClick={() => signOut()}
+            className='flex items-center'
+          >
+            <ArrowRightStartOnRectangleIcon className='w-5 h-5'/>
+            <span>Logout</span>
+          </button>
+
+        </div>
       </div>
     )
 
