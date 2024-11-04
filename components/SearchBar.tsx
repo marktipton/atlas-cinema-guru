fdcdf6d95c6453dc224f9b5494c8e889ced06b05
marktipton <marktipton95@gmail.com>
2024-11-03 18:16:52 -0600

    spacing of search amd min and max year close to perfect

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,36 +3,40 @@ import React, { useState } from 'react';
 const SearchBar = () => {
   const [search, setSearch] = useState('');
   return (
-    <div className='flex justify-between items-start w-full'>
-      <div className='flex flex-col items-start justify-between'>
-        <div>
-          <div>
+    <div className='flex justify-between items-start w-full pb-4'>
+      <div className='flex flex-col items-start justify-between w-1/4'>
+        <div className='w-full'>
+          <div className='flex-grow'>
             Search
           </div>
           <input
             value={search}
             onChange={e => setSearch(e.target.value)}
             placeholder='Search Movies...'
-            className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
+            className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full w-full'
           />
         </div>
-        <div className='flex'>
-          <div>
-            Min Year
+        <div className='flex gap-4 mt-2 w-full'>
+          <div className='flex-grow'>
+            <div>
+              Min Year
+            </div>
+            <input
+                type="number"
+                placeholder=''
+                className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full w-full'
+            />
           </div>
-          <input
+          <div className='flex-grow'>
+            <div>
+              Max Year
+            </div>
+            <input
               type="number"
               placeholder=''
-              className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
-          />
-          <div>
-            Max Year
+              className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full w-full'
+            />
           </div>
-          <input
-            type="text"
-            placeholder=''
-            className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
-          />
         </div>
       </div>
       <div>
