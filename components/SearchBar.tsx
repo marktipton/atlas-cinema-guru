06f40645bb809e2116dbefa82375f5888d8d9bb1
marktipton <marktipton95@gmail.com>
2024-11-03 18:03:24 -0600

    some ruimentary styling for searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,24 +3,36 @@ import React, { useState } from 'react';
 const SearchBar = () => {
   const [search, setSearch] = useState('');
   return (
-    <div>
-      <div>
+    <div className='flex justify-between items-start w-full'>
+      <div className='flex flex-col items-start justify-between'>
         <div>
-          Search
+          <div>
+            Search
+          </div>
+          <input
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            placeholder='Search Movies...'
+            className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
+          />
         </div>
-        <input
-          value={search}
-          onChange={e => setSearch(e.target.value)}
-          placeholder='Search Movies...'
-          className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
-        />
-        <div>
+        <div className='flex'>
           <div>
             Min Year
           </div>
+          <input
+              type="number"
+              placeholder=''
+              className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
+          />
           <div>
             Max Year
           </div>
+          <input
+            type="text"
+            placeholder=''
+            className='bg-searchBlue outline outline-2 p-2 outline-teal rounded-full'
+          />
         </div>
       </div>
       <div>
