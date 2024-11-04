8bd9c288de953d06447404fad664cae288b8d904
marktipton <marktipton95@gmail.com>
2024-11-04 16:15:49 -0600

    populated genres in searchbar

--- components/Genres.tsx
+++ components/Genres.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
 
-const Genres = () => {
+import React, { useEffect, useState } from 'react';
+
+const Genres: React.FC = () => {
+  const [genres, setGenres] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const response = await fetch('/api/genres'); // Fetch from your API
+      const data = await response.json();
+      setGenres(data.genres); // Assuming the response has { genres: [...] }
+    };
+
+    fetchGenres();
+  }, []);
+
   return (
-    <div>Genres</div>
-  )
-}
+    <div>
+      <div>Genres</div>
+      <div className="flex flex-wrap gap-2">
+        {genres.map((genre) => (
+          <div key={genre} className="border border-teal rounded-full px-4 py-1 text-white">
+            {genre}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Genres
+export default Genres;
